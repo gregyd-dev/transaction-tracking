f05e456bb04ea02c68086ae2fc03817e523cf62e
Gregory Deng <gregorydeng@hotmail.com>
2024-06-01 17:02:36 +1000

    Changed again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router basename="/transaction-tracking">
-      <Switch>
+      <Routes>
         <Route exact path="/" component={logo} />
         <Route exact path="/transaction-tracking" component={logo} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
