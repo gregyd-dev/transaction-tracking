36ff5ab6483f26f26594ee3c5d3e1ab5edd250e3
Gregory Deng <gregorydeng@hotmail.com>
2024-06-01 17:15:05 +1000

    Changed again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage'; // Import the created component
-import TransactionTracking from './components/TransactionTracking'; 
 
 function App() {
   return (
-    <Router basename="/transaction-tracking">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/transaction-tracking" element={<TransactionTracking />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
