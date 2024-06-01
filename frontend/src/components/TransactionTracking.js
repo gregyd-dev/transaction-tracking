49e2e1619cb301d1dea6f6390ab65f85c542b4c9
Gregory Deng <gregorydeng@hotmail.com>
2024-06-01 17:07:42 +1000

    Changed App.js

--- frontend/src/components/TransactionTracking.js
+++ frontend/src/components/TransactionTracking.js
@@ -3,7 +3,6 @@ import React from 'react';
 function TransactionTracking() {
   return (
     <div>
-      <img src={logo} alt="logo" />
       <h1>Welcome to Transaction Tracking</h1>
     </div>
   );
