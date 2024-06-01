21c4fe4ca0ea4ca1081a6971af0f8ec467ecb61f
Gregory Deng <gregorydeng@hotmail.com>
2024-06-01 17:29:00 +1000

    Changes

---
+++ frontend/src/TransactionTracking.js
@@ -0,0 +1,10 @@
+function TransactionTracking() {
+  return (
+    <div>
+        <div>Hoi scobie! Press the button to see scregorie's latest transaction on Up:</div>
+        <button type="button">Button</button>
+    </div>
+  );
+}
+
+export default TransactionTracking;
